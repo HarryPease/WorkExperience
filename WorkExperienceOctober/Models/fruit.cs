07952fe0c48a06e14c079ffc17d9fb4de214dbc9
Harry Pease <hpease@rdt.co.uk>
2023-10-22 23:00:26 +0000

    Merged PR 4735: Initial Commit
    Initial Commit

---
+++ WorkExperienceOctober/Models/fruit.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkExperienceOctober.Models
+{
+    public class Fruit
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public string family { get; set; }
+        public string order { get; set; }
+        public string genus { get; set; }
+        public Nutrition nutritions { get; set; }
+    }
+}
---
+++ WorkExperienceOctober/Models/nutrition.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkExperienceOctober.Models
+{
+    public class Nutrition
+    {
+        public int calories { get; set; }
+        public double fat { get; set; }
+        public double sugar { get; set; }
+        public double carbohydrates { get; set; }
+        public double protein { get; set; }
+    }
+}
